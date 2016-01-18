9057da8d759de2c1a623ee52c008d01a3f156e50
Merce Montoliu Nerin <merce.montoliu@gmail.com>
2016-01-18 11:39:08 +0100

    new arguments added

--- BLAST_parse/BLAST_parse.py
+++ BLAST_parse/BLAST_parse.py
@@ -8,19 +8,31 @@ parser = argparse.ArgumentParser(description='Blast parser for blastn outputs in
 
 #Input argument added, to be able to autocomplete use chmod u+x
 
-parser.add_argument('-i','--input', help='Input table from blast output', required=True)
+parser.add_argument('-i','--input', help='Input table from blast output - Required', required=True)
 
-"""Alignment length argument added, 
-default 0 means all blast results will be selected. 
-type float to allow decimals.""" 
+#Alignment length argument added, 
+#default 0 means all blast results will be selected. 
+#type float to allow decimals.
 
-parser.add_argument('-a','--alignment_length', help='minimum alignment length', default=0, type=float)
+parser.add_argument('-a','--alignment_length', help='minimum alignment length - Default 0', default=0, type=float)
 
-"""Percentage of identity argument added, 
-default 0 means all blast results will be selected. 
-type float to allow decimals."""
+#Percentage of identity argument added, 
+#default 0 means all blast results will be selected. 
+#type float to allow decimals.
 
-parser.add_argument('-p', '--identity', help='minimum percentage identity', default=0, type=float)
+parser.add_argument('-p', '--identity', help='minimum percentage of identity - Default 0', default=0, type=float)
+
+#E-value argument added, 
+#default 100 means all blast results will be selected. 
+#type float to allow decimals.
+
+parser.add_argument('-e', '--e_value', help='e-value cutoff - Default 100', default=100, type=float)
+
+#Bit score argument added, 
+#default 0 means all blast results will be selected. 
+#type float to allow decimals.
+
+parser.add_argument('-b', '--bitscore', help='bit score cutoff - Default 0', default=0, type=float)
 
 #Output file argument added
 
@@ -34,8 +46,8 @@ args=parser.parse_args()
 
 table=pd.read_table(args.input, sep='\t', header=None)
 
-parsed_table=table[(table[3] > args.alignment_length) & (table[2] > args.identity)]
+parsed_table=table[(table[3] > args.alignment_length) & (table[2] > args.identity) & (table[10] < args.e_value) & (table[11] > args.bitscore)]
 
 parsed_table.to_csv(args.output, header=None, index=False, sep='\t')
 
-#si no funciona ./ (lo de arriba)
+
